7b0566f6326ba367ac7bbf1842d652df8151156c
Nathan Mugerwa <natha1@N-Ms-MBP.fios-router.home>
2022-05-09 19:40:30 -0400

    Clean up code and add visualization logic

--- src/model.py
+++ src/model.py
@@ -1,6 +1,3 @@
-import time
-import timeit
-
 import tensorflow as tf
 from keras.initializers.initializers_v1 import TruncatedNormal
 from tensorflow.keras.layers import Conv2D, Conv2DTranspose, BatchNormalization
@@ -10,21 +7,13 @@ class ColorizationModel(tf.keras.Model):
     def __init__(self):
         super(ColorizationModel, self).__init__()
 
-        # TODO
         # 1) Define any hyperparameters, optimizer, ...
-
-        # Hyperparameters
         self.learning_rate = 0.00001
         self.batch_size = 40  # we can test different batch sizes
-        self.l_cent = 50
-        self.l_norm = 100
-        self.ab_norm = 110
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
         self.mse = tf.keras.losses.MeanSquaredError()
         self.stdev = 0.04
-        self.init_time = timeit.default_timer()
 
-        # TODO
         # 2) Define layers
         kernel_init = TruncatedNormal(stddev=self.stdev)
 
@@ -99,19 +88,10 @@ class ColorizationModel(tf.keras.Model):
                               kernel_initializer=kernel_init, activation='relu'))
         self.model.add(BatchNormalization())
 
-        # # I'm not sure if these are the correct layers?
-        # self.model.add(Conv2D(filters=2, kernel_size=1, strides=1, padding="valid", bias=False))
-        # self.model.add(UpSampling2D(size=(2, 2), interpolation='bilinear'))
-
         # conv9 section
         self.model.add(Conv2DTranspose(filters=256, kernel_size=4, strides=4, padding="SAME",
                                        kernel_initializer=kernel_init, activation='relu'))
         self.model.add(BatchNormalization())
-
-        # TODO: Remove or keep, if my changes are wrong
-        # y_hat
-        # self.model.add(Conv2D(filters=2, kernel_size=1, strides=1, dilation_rate=1, padding="same",
-        #                       kernel_initializer=kernel_init, activation='softmax'))
 
         # Removed the softmax activation because we no longer want probabilities
         self.model.add(Conv2D(filters=2, kernel_size=1, strides=1, dilation_rate=1, padding="same",
@@ -123,10 +103,7 @@ class ColorizationModel(tf.keras.Model):
         :param inputs: Black-and-white images, in L*a*b space. Dimensions of (batch_size, 32, 32, 1)
         :return The predicted (a, b) channels for each batch image. Dimensions of (batch_size, 32, 32, 2)
         """
-        out = self.model(inputs)
-        # print("DEBUG!")
-        # print(out.shape)
-        return out
+        return self.model(inputs)
 
     def loss_function(self, predictions, labels):
         """
@@ -134,11 +111,4 @@ class ColorizationModel(tf.keras.Model):
         :param labels: The true values of the (a, b) channels of each batch image
         :return: the MSE of the values in the predicted vs true channels
         """
-        loss = self.mse(labels, predictions)
-        curr_time = timeit.default_timer()
-        num_min_since_train_start = (curr_time - self.init_time) / 60
-        # TODO: training takes a really, really long time with this current design
-        # print(f"Time elapsed since training started: {num_min_since_train_start} minutes")
-        return loss
-
-        # num_imgs,h,w = predictions.shape[0],predictions.shape[1],predictions.shape[2]
+        return self.mse(labels, predictions)
